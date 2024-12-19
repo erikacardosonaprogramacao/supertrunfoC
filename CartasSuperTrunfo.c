67b7b05f070436e11a00d1053187e7afd27db6dc
AlexSG1709 <programadoralex.github@gmail.com>
2024-12-19 16:27:46 +0000

    pais, cidade.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,23 +9,20 @@ int main() {
     // Declarando variáveis para armazenar países.
     char País_A[30], País_B[30], País_C[30],País_D[30], País_E[30], País_F[30], País_G[30], País_H[30];
 
-    // Declarando variáveis para armazenar as cidades.
-    char cidade_A01[30], cidade_A02[30], cidade_A03[30], cidade_A04[30], cidade_B01[30], cidade_B02[30], cidade_B03[30], cidade_B04[30];
-    char cidade_C01[30], cidade_C02[30], cidade_C03[30], cidade_C04[30], cidade_D01[30], cidade_D02[30], cidade_D03[30], cidade_D04[30];
-    char cidade_E01[30], cidade_E02[30], cidade_E03[30], cidade_E04[30], cidade_F01[30], cidade_F02[30], cidade_F03[30], cidade_F04[30];
-    char cidade_G01[30], cidade_G02[30], cidade_G03[30], cidade_G04[30], cidade_H01[30], cidade_H02[30], cidade_H03[30], cidade_H04[30];
+    // Declarando variáveis para armazenar os estados.
+    char estado_A01[30], estado_A02[30], estado_A03[30], estado_A04[30], estado_B01[30], estado_B02[30], estado_B03[30], estado_B04[30];
+    char estado_C01[30], estado_C02[30], estado_C03[30], estado_C04[30], estado_D01[30], estado_D02[30], estado_D03[30], estado_D04[30];
+    char estado_E01[30], estado_E02[30], estado_E03[30], estado_E04[30], estado_F01[30], estado_F02[30], estado_F03[30], estado_F04[30];
+    char estado_G01[30], estado_G02[30], estado_G03[30], estado_G04[30], estado_H01[30], estado_H02[30], estado_H03[30], estado_H04[30];
 
     //Defina as variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    char cod_estado_A01[4], cod_estado_A02[4], cod_estado_A03[4], cod_estado_A04[4], cod_estado_B01[4], cod_estado_B02[4], cod_estado_B03[4], cod_estado_B04[4];
+    char cod_estado_C01[4], cod_estado_C02[4], cod_estado_C03[4], cod_estado_C04[4], cod_estado_D01[4], cod_estado_D02[4], cod_estado_D03[4], cod_estado_D04[4];
+    char cod_estado_E01[4], cod_estado_E02[4], cod_estado_E03[4], cod_estado_E04[4], cod_estado_F01[4], cod_estado_F02[4], cod_estado_F03[4], cod_estado_F04[4];
+    char cod_estado_G01[4], cod_estado_G02[4], cod_estado_G03[4], cod_estado_G04[4], cod_estado_H01[4], cod_estado_H02[4], cod_estado_H03[4], cod_estado_H04[4];
+    int população_A01, população_A02, população_A03, população_A04, população_B01, 
     
-    int cidade;
-    char nome[20];
-    int população;
-    float área_km²;
-    float PIB;
-    int ponto_turístico;
-    
-
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
