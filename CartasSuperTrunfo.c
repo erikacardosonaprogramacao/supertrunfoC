d2f303f695472981f59ed6c9e74737bdce6226a8
AlexSG1709 <programadoralex.github@gmail.com>
2024-12-19 17:30:50 +0000

    atributos1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,7 +21,20 @@ int main() {
     char cod_estado_C01[4], cod_estado_C02[4], cod_estado_C03[4], cod_estado_C04[4], cod_estado_D01[4], cod_estado_D02[4], cod_estado_D03[4], cod_estado_D04[4];
     char cod_estado_E01[4], cod_estado_E02[4], cod_estado_E03[4], cod_estado_E04[4], cod_estado_F01[4], cod_estado_F02[4], cod_estado_F03[4], cod_estado_F04[4];
     char cod_estado_G01[4], cod_estado_G02[4], cod_estado_G03[4], cod_estado_G04[4], cod_estado_H01[4], cod_estado_H02[4], cod_estado_H03[4], cod_estado_H04[4];
-    int população_A01, população_A02, população_A03, população_A04, população_B01, 
+    int população_A01, população_A02, população_A03, população_A04, população_B01, polulação_B02, população_B03, população_B04;
+    int população_C01, população_C02, população_C03, população_C04, população_D01, população_D02, população_D03, população_D04;
+    int população_E01, população_E02, população_E03, população_E04, população_F01, população_F02, população_F03, população_F04;
+    int população_G01, população_G02, população_G03, população_G04, população_H01, população_H02, população_H03, população_H04;
+    float área_km²_A01, área_km²_A02, área_km²_A03, área_km²_A04, área_km²_B01, área_km²_B02, área_km²_B03, área_km²_B04;
+    float área_km²_C01, área_km²_C02, área_km²_C03, área_km²_C04, área_km²_D01, área_km²_D02, área_km²_D03, área_km²_D04;
+    float área_km²_E01, área_km²_E02, área_km²_E03, área_km²_E04, área_km²_F01, área_km²_F02, área_km²_F03, área_km²_F04;
+    float área_km²_G01, área_km²_G02, área_km²_G03, área_km²_G04, área_km²_H01, área_km²_H02, área_km²_H03, área_km²_H04;
+    float PIB_A01, PIB_A02, PIB_A03, PIB_A04, PIB_B01, PIB_B02, PIB_B03, PIB_B04, PIB_C01, PIB_C02, PIB_C03, PIB_C04, PIB_D01, PIB_D02, PIB_D03, PIB_D04;
+    float PIB_E01, PIB_E02, PIB_E03, PIB_E04, PIB_F01, PIB_F02, PIB_F03, PIB_F04, PIB_G01, PIB_G02, PIB_G03, PIB_G04, PIB_H01, PIB_H02, PIB_H03, PIB_H04;
+    int pontos_turísticos_A01, pontos_turísticos_A02, pontos_turísticos_A03, pontos_turísticos_A04, pontos_turísticos_B01, pontos_turísticos_B02, pontos_turísticos_B03, pontos_turísticos_B04;
+    int pontos_turísticos_C01, pontos_turísticos_C02, pontos_turísticos_C03, pontos_turísticos_C04, pontos_turísticos_D01, pontos_turísticos_D02, pontos_turísticos_D03, pontos_turísticos_D04;
+    int pontos_turísticos_E01, pontos_turísticos_E02, pontos_turísticos_E03, pontos_turísticos_E04, pontos_turísticos_F01, pontos_turísticos_F02, pontos_turísticos_F03, pontos_turísticos_F04;
+    int pontos_turísticos_G01, pontos_turísticos_G02, pontos_turísticos_G03, pontos_turísticos_G04, pontos_turísticos_H01, pontos_turísticos_H02, pontos_turísticos_H03, pontos_turísticos_H04;
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
