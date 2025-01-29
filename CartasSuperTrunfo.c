1a56a4d7821e50b701c56e1696b6f0750cbabc01
bielobrabo <gabriel.o.g.macedo1@gmail.com>
2025-01-29 03:08:38 +0000

    Fiz o printf e o scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,5 +2,35 @@
 
 int main() {  
     printf("Desafio SuperTriunfo!\n");
-    return 0;  // Added return statement
-}     
+
+    char estado[50];
+    char codigo_da_carta[50];
+    char nome_da_cidade[50];
+    int população;
+    float area_em_km2;
+    float pib;
+    int numeros_de_pontos_turisticos;
+    
+    printf("Digite seu estado: \n");
+    scanf("%s", estado);
+    
+    printf("Digite o código da carta: \n");
+    scanf("%s", codigo_da_carta);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", nome_da_cidade);
+
+    printf("Digite a populacão: \n");
+    scanf("%d", &população);
+
+    printf("Digite a área em km2: \n");
+    scanf("%f", &area_em_km2);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &pib);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d", &numeros_de_pontos_turisticos);
+    
+    return 0;
+}
