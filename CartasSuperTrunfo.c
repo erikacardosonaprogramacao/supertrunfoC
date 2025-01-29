7d3eab9714f10ba43fd3b095e1d6f10e86ec8153
bielobrabo <gabriel.o.g.macedo1@gmail.com>
2025-01-29 03:50:48 +0000

    refiz os codigos da forma certa

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
-
+#include <string.h>
 int main() {  
     printf("Desafio SuperTriunfo!\n");
 
     char estado[50];
     char codigo_da_carta[50];
     char nome_da_cidade[50];
-    int população;
+    int populacao;
     float area_em_km2;
     float pib;
     int numeros_de_pontos_turisticos;
     
     printf("Digite seu estado: \n");
-    scanf("%s", estado);
-    
+    fgets(estado, sizeof(estado), stdin);
+    estado[strcspn(estado, "\n")] = '\0';  
+
     printf("Digite o código da carta: \n");
-    scanf("%s", codigo_da_carta);
-
+    fgets(codigo_da_carta, sizeof(codigo_da_carta), stdin);
+    codigo_da_carta[strcspn(codigo_da_carta, "\n")] = '\0';  
+   
     printf("Digite o nome da cidade: \n");
-    scanf("%s", nome_da_cidade);
+    fgets(nome_da_cidade, sizeof(nome_da_cidade), stdin);
+    nome_da_cidade[strcspn(nome_da_cidade, "\n")] = '\0';  
 
-    printf("Digite a populacão: \n");
-    scanf("%d", &população);
+    printf("Digite a população: \n");
+    scanf("%d", &populacao);
 
     printf("Digite a área em km2: \n");
     scanf("%f", &area_em_km2);
@@ -31,6 +34,6 @@ int main() {
 
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &numeros_de_pontos_turisticos);
-    
+
     return 0;
 }
