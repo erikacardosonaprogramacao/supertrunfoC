c526ce2b52e803ce4621126a81a7325d6741580b
Hyken1 <mauriciols1@outlook.com>
2025-02-02 20:15:53 +0000

    introducao de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos  turísticos.
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
---
+++ Novato.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+//Cadastro de variaveis//
+
+int main(){
+    
+    char nome[5];
+    char codigo[3];
+    float populacao; 
+    float area;
+    float PIB;
+    int turisticos;
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &nome);
+    printf("Cidade: %s\n", &nome);
+
+    printf("Digite o codigo da cidade: ");
+    scanf("%s", &codigo);
+    printf("Codigo: %s\n", &codigo);
+
+    printf("Digite a populacao da cidade: ");
+    scanf("%f", &populacao);
+    printf("Populacao: %f\n", populacao);
+
+    printf("Digite a area da cidade: ");
+    scanf("%f", &area);
+    printf("Area da cidade: %f km\n", area);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &PIB);
+    printf("PIB: %f reais\n");
+
+    printf("Digite o numero de pontos turisiticos: ");
+    scanf("%d", &turisticos);
+    printf("Pontos turisticos: %d",turisticos);
+
+    return 0;
+
+}
