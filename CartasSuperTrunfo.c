61c7e0e3bac889962349586ea746275665de54bc
santxl6 <iansantos943@gmail.com>
2025-02-04 19:28:49 +0000

    teste1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 int main() {
@@ -16,6 +17,9 @@ printf("populacao: %d habitantes\n", populacao);
 printf("area: %d km²\n", area);
 
 
+char codigo2[4] = "B02";
+char nome2[50] = "RIO DE JANEIRO";
+char estado2[1] = "B";
 int populacao2 = 67890123;
 int area2 = 1202;
 
@@ -26,6 +30,18 @@ printf("estado: %s\n", estado2);
 printf("populacao: %d habitantes\n", populacao2);
 printf("area: %d km²\n", area2);
 
+char codigo3[4] = "C03";
+char nome3[50] = "BRASILIA";
+char estado3[1] = "C";
+int populacao3 = 89125634;
+int area3 = 3421;
+
+printf("carta criada com sucesso!\n");
+printf("codigo: %s\n", codigo3);
+printf("nome da cidade: %s\n", nome3);
+printf("estado: %s\n", estado3);
+printf("populacao: %d habitantes\n", populacao3);
+printf("area: %d km²\n", area3);
 
 return 0;
 
