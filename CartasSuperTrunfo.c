d976a78901e957bcf9a7331700fef115a051b1c7
santxl6 <iansantos943@gmail.com>
2025-02-04 19:24:37 +0000

    3 cidades

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,14 +12,10 @@ printf("carta criada com sucesso!\n");
 printf("codigo: %s\n", codigo);
 printf("nome da cidade: %s\n", nome);
 printf("estado: %s\n", estado);
-printf("populacao: %d habitantes\n", populaçao);
+printf("populacao: %d habitantes\n", populacao);
 printf("area: %d km²\n", area);
 
-("\n-----------------------------\n");
 
-char codigo2[4] = "B02";
-char nome2[50] = "RIO DE JANEIRO";
-char estado2[1] = "B";
 int populacao2 = 67890123;
 int area2 = 1202;
 
@@ -29,6 +25,7 @@ printf("nome da cidade: %s\n", nome2);
 printf("estado: %s\n", estado2);
 printf("populacao: %d habitantes\n", populacao2);
 printf("area: %d km²\n", area2);
+
 
 return 0;
 
