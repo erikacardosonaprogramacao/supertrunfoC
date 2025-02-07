72f1377ac05aac817a791a800371a1219437f146
cal-ves <calves.dev@gmail.com>
2025-02-07 19:14:37 -0300

    Refatoração do código e adição de menu

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -35,68 +35,89 @@ struct pais
     struct estado estados[8];
 };
 
+// Declaração de array estático para armazenar a quantidade de países
+struct pais paises[1];
+
+// Declaração de variáveis
+int opcao;
+int qtd_estados = 8;
+int qtd_cidades = 4;
+char *cod_estado[8] = {"A", "B", "C", "D", "E", "F", "G", "H"};
+char *cod_cidade[4] = {"01", "02", "03", "04"};
+
+// Função para cadastrar as informações das cartas
+void cadastrarCartas()
+{
+    printf("Vamos começar cadastrando as informações das cidades.\n");
+    for (int j = 0; j < qtd_estados; j++)
+    {
+        for (int k = 0; k < qtd_cidades; k++)
+        {
+            printf("\nCidade %s%s\n", cod_estado[j], cod_cidade[k]);
+            printf("Digite a população da cidade %d: ", k + 1);
+            scanf("%d", &paises[0].estados[j].cidades[k].populacao);
+            printf("Digite a área da cidade %d: ", k + 1);
+            scanf("%d", &paises[0].estados[j].cidades[k].area);
+            printf("Digite o PIB da cidade %d: ", k + 1);
+            scanf("%d", &paises[0].estados[j].cidades[k].pib);
+            printf("Digite a quantidade de pontos turísticos da cidade %d: ", k + 1);
+            scanf("%d", &paises[0].estados[j].cidades[k].pontos_turisticos);
+        }
+    }
+    printf("\nAs informações das cidades foram cadastradas com sucesso!\n");
+}
+
+// Função para exibir as informações das cartas cadastradas
+void exibeCartas()
+{
+
+    for (int j = 0; j < qtd_estados; j++)
+    {
+        printf("\nEstado %s\n", cod_estado[j]);
+        for (int k = 0; k < qtd_cidades; k++)
+        {
+            printf("\nCidade %s%s\n", cod_estado[j], cod_cidade[k]);
+            printf("População: %d\n", paises[0].estados[j].cidades[k].populacao);
+            printf("Área: %dm²\n", paises[0].estados[j].cidades[k].area);
+            printf("PIB: R$ %d\n", paises[0].estados[j].cidades[k].pib);
+            printf("Pontos turísticos: %d\n", paises[0].estados[j].cidades[k].pontos_turisticos);
+        }
+    }
+}
+
 int main()
 {
 
-    // Declaração de variáveis
-    int qtd_paises = 1;
-    int qtd_estados = 8;
-    int qtd_cidades = 4;
-    char *cod_estado[8] = {"A", "B", "C", "D", "E", "F", "G", "H"};
-    char *cod_cidade[4] = {"01", "02", "03", "04"};
-
-    // Mensagem de boas-vindas
-    printf("Bem-vindo ao jogo de Super Trunfo com o tema Países!\n");
-    printf("Cada país será divido em 8 estados com 4 cidades cada.\n");
-    printf("Por exemplo, a cidade A01 é a cidade 1 do estado A.\n");
-    printf("Vamos começar cadastrando as informações das cidades.\n");
-
-    // Declaração de array estático para armazenar a quantidade de países
-    struct pais paises[1];
-
-    // Laço para cadastrar as informações das cartas cadastradas
-    for (int i = 0; i < qtd_paises; i++)
+    do
     {
-        printf("\nDigite o nome do país %d: ", i + 1);
-        scanf("%s", paises[i].nome);
+        // Mensagem de boas-vindas
+        printf("Bem-vindo ao jogo de Super Trunfo com o tema Países!\n");
+        printf("1 - Para começar o jogo\n");
+        printf("2 - para ler as regras do jogo\n");
+        printf("3 - para sair do jogo\n");
+        scanf("%d", &opcao);
 
-        for (int j = 0; j < qtd_estados; j++)
+        switch (opcao)
         {
-            printf("\nDigite o nome do estado %d: ", j + 1);
-            scanf("%s", paises[i].estados[j].nome);
-
-            for (int k = 0; k < qtd_cidades; k++)
-            {
-                printf("\nCidade %s%s\n", cod_estado[j], cod_cidade[k]);
-                printf("Digite a população da cidade %d: ", k + 1);
-                scanf("%d", &paises[i].estados[j].cidades[k].populacao);
-                printf("Digite a área da cidade %d: ", k + 1);
-                scanf("%d", &paises[i].estados[j].cidades[k].area);
-                printf("Digite o PIB da cidade %d: ", k + 1);
-                scanf("%d", &paises[i].estados[j].cidades[k].pib);
-                printf("Digite a quantidade de pontos turísticos da cidade %d: ", k + 1);
-                scanf("%d", &paises[i].estados[j].cidades[k].pontos_turisticos);
-            }
+        case 1:
+            cadastrarCartas();
+            exibeCartas();
+            break;
+        case 2:
+            printf("\n\nCada país será divido em 8 estados com 4 cidades cada.\n");
+            printf("Por exemplo, a cidade A01 é a cidade 1 do estado A.\n");
+            printf("Cada cidade possui informações como população, área, PIB e quantidade de pontos turísticos.\n");
+            printf("O jogador que tiver a carta com a maior população, área, PIB ou quantidade de pontos turísticos ganha o jogo.\n");
+            printf("Divirta-se!\n\n");
+            break;
+        case 3:
+            printf("Obrigado por jogar! Até a próxima!\n");
+            break;
+        default:
+            printf("Opção inválida! Tente novamente.\n\n\n");
+            break;
         }
-    }
+    } while (opcao != 3);
 
-    // Laço para exibir as informações das cartas cadastradas
-    printf("\nAs informações dos países foram cadastradas com sucesso!\n");
-    printf("Agora é hora de jogar!\n");
-    for (int i = 0; i < qtd_paises; i++)
-    {
-        printf("\nPaís %d: %s\n", i + 1, paises[i].nome);
-        for (int j = 0; j < qtd_estados; j++)
-        {
-            printf("\nEstado %s\n", cod_estado[j]);
-            for (int k = 0; k < qtd_cidades; k++)
-            {
-                printf("\nCidade %s%s\n", cod_estado[j], cod_cidade[k]);
-                printf("População: %d\n", paises[i].estados[j].cidades[k].populacao);
-                printf("Área: %dm²\n", paises[i].estados[j].cidades[k].area);
-                printf("PIB: R$%d\n", paises[i].estados[j].cidades[k].pib);
-                printf("Pontos turísticos: %d\n", paises[i].estados[j].cidades[k].pontos_turisticos);
-            }
-        }
-    }
+    return 0;
 }
