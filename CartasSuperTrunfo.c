fc7e22dd9248cb83c2c2a8d74de8b136677f12ff
Daniel Souza <131969762+DanielDosDiv@users.noreply.github.com>
2025-02-12 09:39:25 -0300

    Desafio CartasSuperTrunfo Nível Novato.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    int Populacao;
+    float Area;
+    double PIB;
+    int Pontos_Turisticos;
+
+    printf("\n-------- SUPER TRUNFO --------\n");
+
+    // Cadastrando a Carta
+    printf("Insira a quantidade populacional do país: ");
+    scanf("%d", &Populacao);  
+
+    printf("Insira a Área do país (em km²): ");
+    scanf("%f", &Area);  
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Insira o PIB do país (em trilhões): ");
+    scanf("%lf", &PIB);  
+
+    printf("Insira a quantidade de pontos turísticos: ");
+    scanf("%d", &Pontos_Turisticos);  
+
+    // Fim do cadastro
+    printf("\nCarta cadastrada com sucesso!!\n");
+
+    // Exibindo valores cadastrados
+    printf("População do país: %d habitantes\n", Populacao);
+    printf("Área do país: %.2f km²\n", Area);
+    printf("PIB do país: %.2lf trilhões\n", PIB);
+    printf("Pontos turísticos: %d\n", Pontos_Turisticos);
 
     return 0;
 }
