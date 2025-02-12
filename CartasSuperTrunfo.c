6f81580e4f75a7370dff7cda5a77e59a55d94a25
Daniel Souza <131969762+DanielDosDiv@users.noreply.github.com>
2025-02-12 10:21:21 -0300

    Desafio nível Aventureiro CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,8 @@ int main() {
     float Area;
     double PIB;
     int Pontos_Turisticos;
+    float Densidade_Populacional;
+    double PIB_per_capita;
 
     printf("\n-------- SUPER TRUNFO --------\n");
 
@@ -21,14 +23,18 @@ int main() {
     printf("Insira a quantidade de pontos turísticos: ");
     scanf("%d", &Pontos_Turisticos);  
 
-    // Fim do cadastro
-    printf("\nCarta cadastrada com sucesso!!\n");
+    // Cálculo das métricas
+    Densidade_Populacional = (float)Populacao / Area;  
+    PIB_per_capita = (PIB * 1e12) / Populacao;  
 
-    // Exibindo valores cadastrados
+    // Exibindo informações
+    printf("\n--------  Informações da sua Carta  --------\n");
     printf("População do país: %d habitantes\n", Populacao);
     printf("Área do país: %.2f km²\n", Area);
     printf("PIB do país: %.2lf trilhões\n", PIB);
     printf("Pontos turísticos: %d\n", Pontos_Turisticos);
+    printf("Densidade Populacional: %.2f habitantes/km²\n", Densidade_Populacional);
+    printf("PIB per Capita: %.2lf reais\n", PIB_per_capita);
 
     return 0;
 }
