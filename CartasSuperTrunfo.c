956e6205083c1191ec69515496d90119c56c72dd
Maurinho2025 <mauromaria1@gmail.com>
2025-02-12 14:04:50 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,48 +1,67 @@
 #include <stdio.h>
-#include <locale.h>
+#include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
+// Estrutura para armazenar os dados de uma cidade
+typedef struct {
+    char nome[50];
+    int populacao;
+    float area;
+    float pib;
+} Cidade;
 
 int main() {
-    setlocale(LC_ALL, "en_US.UTF-8");
-    // Definição das variáveis para os atributos da cidade
-    int codigo;
-    char nome[100]; // Nome com no máximo 99 caracteres + o terminador nulo
-    long long populacao;
-    double area;
-    double pib;
-    int pontos_turisticos;
-
-    // Cadastro das Cartas
-    printf("Cadastro de Cartas de Cidades\n");
-
-    printf("Código da cidade: ");
-    scanf("%d", &codigo);
-
-    printf("Nome da cidade: ");
-    scanf(" %s[^\n]", nome); // Lê o nome até a quebra de linha
-
-    printf("População: ");
-    scanf("%lld", &populacao);
-
-    printf("Área (km²): ");
-    scanf("%lf", &area);
-
-    printf("PIB: ");
-    scanf("%lf", &pib);
-
-    printf("Número de pontos turísticos: ");
-    scanf("%d", &pontos_turisticos);
-
-    // Exibição dos Dados das Cartas
-    printf("\nDados da Carta Cadastrada:\n");
-    printf("Código: %d\n", codigo);
-    printf("Nome: %s\n", nome);
-    printf("População: %lld\n", populacao);
-    printf("Área: %.2lf km²\n", area); // Formatação para 2 casas decimais
-    printf("PIB: %.2lf\n", pib); // Formatação para 2 casas decimais
-    printf("Pontos Turísticos: %d\n", pontos_turisticos);
-
+    Cidade cidade1, cidade2;
+    int escolha;
+    
+    // Cadastro das cidades
+    printf("Digite o nome da primeira cidade: ");
+    scanf("%s", cidade1.nome);
+    printf("Digite a população da primeira cidade: ");
+    scanf("%d", &cidade1.populacao);
+    printf("Digite a área da primeira cidade (em km²): ");
+    scanf("%f", &cidade1.area);
+    printf("Digite o PIB da primeira cidade (em bilhões): ");
+    scanf("%f", &cidade1.pib);
+    
+    printf("\nDigite o nome da segunda cidade: ");
+    scanf("%s", cidade2.nome);
+    printf("Digite a população da segunda cidade: ");
+    scanf("%d", &cidade2.populacao);
+    printf("Digite a área da segunda cidade (em km²): ");
+    scanf("%f", &cidade2.area);
+    printf("Digite o PIB da segunda cidade (em bilhões): ");
+    scanf("%f", &cidade2.pib);
+    
+    // Escolha do critério de comparação
+    printf("\nEscolha um critério para comparar:\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - PIB\n");
+    printf("Digite sua escolha: ");
+    scanf("%d", &escolha);
+    
+    // Comparação das cidades
+    if (escolha == 1) {
+        if (cidade1.populacao > cidade2.populacao) {
+            printf("%s venceu com maior população!\n", cidade1.nome);
+        } else {
+            printf("%s venceu com maior população!\n", cidade2.nome);
+        }
+    } else if (escolha == 2) {
+        if (cidade1.area > cidade2.area) {
+            printf("%s venceu com maior área!\n", cidade1.nome);
+        } else {
+            printf("%s venceu com maior área!\n", cidade2.nome);
+        }
+    } else if (escolha == 3) {
+        if (cidade1.pib > cidade2.pib) {
+            printf("%s venceu com maior PIB!\n", cidade1.nome);
+        } else {
+            printf("%s venceu com maior PIB!\n", cidade2.nome);
+        }
+    } else {
+        printf("Opção inválida.\n");
+    }
+    
     return 0;
 }
