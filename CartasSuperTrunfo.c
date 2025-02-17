78b4d7ac8c71266f18522e5dfabde33147e0b3d3
DiegoMarinho27 <diegogmrealcred@gmail.com>
2025-02-17 20:48:44 +0000

    Eu criei printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,9 @@
 //Teste larissa
 
 int main() {
+    printf ("Cartas Super Trunfo");
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -17,6 +20,8 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    
 
     return 0;
 }
