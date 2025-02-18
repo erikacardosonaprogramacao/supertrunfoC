463f15d2a2c64a548b3cf809b4f47e9c071b27a0
DiegoMarinho27 <diegogmrealcred@gmail.com>
2025-02-18 02:36:13 +0000

    construção

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,53 +6,35 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste Diego
 
-int main() {
-          
-    
-    char Estado = 'A';                       // Estado
-    int CodigoCarta [50] = 'A01';            // Código da Carta
-    char NomeCidade[50] = "São Paulo";       // Nome da Cidade
-    long Populacao = 11.45;                  // População
-    float Area = 1.521;                      // Área em km²
-    float PIB = 2.719751;                    // PIB
-    int NumeroPontosTuristicos = 36;         // Número de pontos turísticos
 
-    // Leitura dos dados da carta
 
-    printf ("Digite o Estado: \n");
-    scanf ("%s", &carta.estado);
 
-    printf ("Digite o Código da Carta: \n");
-    scanf ("%d", &carta.codigoCarta);
-
-    printf ("Digite o nome da Cidade: \n");
-    scanf ("%s", &nomeCidade);
-
-    printf("Digite a População: \n");
-    scanf("%ld", &Populacao);
-
-    printf ("Digite a Área em km²: \n");
-    sscanf("%f", &carta.area);
-
-    printf ("PIB: \n");
-    scanf("%f", &carta.PIB);
-
-    printf("Número de Ponto Turísticos: \n");
-    scanf("%d", &carta.numPontosTurísticos);
-
-    printf ("Cartas Super Trunfo\n");   
+int main(){
+    printf ("Cartas Super Trunfo\n");
     printf ("Novo Commint\n");
-    printf ("Estado: %s\n, carta.estado");                  
-    printf ("Código da Carta: %d\n", carta.codigoCarta);   
-    printf ("Nome da Cidade; %s\n", carta.nomeCidade);     
-    printf ("População: %ld", carta.populacao);          
-    printf ("Área: %.2f km²\n", carta.area);             
-    printf ("PIB: %.2f\n, carta.pib");                
-    printf("Número de Pontos Turísticos: %d\n", carta.numPontosTuristicos);
+
+    int Carta = 1;
+    char Estado = 'A';
+    float Codigo[12] = 'A01;
+    char nome [20] = "São Paulo";
 
 
+    printf("Carta: %d\n", Carta);
+    printf("Estado: %c\n", Estado);
+    printf("Codigo: %d\n", Codigo);
+    printf("Nome: %s\n", Nome);
 
-    
+ 
+    /*
+   printf("%formato1 %formato2 %formato3 %formato4 %formato5 %formato6 %formato7 %formato8", variavel1 variavel2, variavel3, variavel4, variavel5, variavel6, variavel7, variavel8);
+
+    %d: Imprime um inteiro no formato decimal.
+    %i: Equivalente a %d.
+    %f: Imprime um número de ponto flutuante no formato parão.
+    %e: Imprime um número de ponto flutuante na notação científica.
+    %C: Imprime um único caractere.
+    %s: Imprime uma cadeia (string) de caracteres.
+
 
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
@@ -65,8 +47,22 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    
+    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    
+    // Cadastro das Cartas:
+    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
+    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    
+    // Exibição dos Dados das Cartas:
+    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
+    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    */
 
     
 
     return 0;
+
 }
+
---
+++ CartasSuperTrunfo.h
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main(){
+
+    printf ("Cartas Super Trunfo\n");
+    printf ("Novo Commint\n");
+
+    int Carta = 1;
+    char Estado = 'A';
+    float Codigo = 'A01';
+    char NomedaCidade[50] = "São Paulo";
+
+    printf("Carta: %e\n", Carta);
+    printf("Estado: %c\n", Estado);
+    printf("Codigo: %ls\n", Codigo);
+    printf("NomedaCidade: %s\n", Cidade);
+
+
+     /*
+   printf("%formato1 %formato2 %formato3 %formato4 %formato5 %formato6 %formato7 %formato8", variavel1 variavel2, variavel3, variavel4, variavel5, variavel6, variavel7, variavel8);
+
+    %d: Imprime um inteiro no formato decimal.
+    %i: Equivalente a %d.
+    %f: Imprime um número de ponto flutuante no formato parão.
+    %e: Imprime um número de ponto flutuante na notação científica.
+    %C: Imprime um único caractere.
+    %s: Imprime uma cadeia (string) de caracteres.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
+   
