f202b06a2f89fb68ddec661a199788d6044f2ddc
DiegoMarinho27 <diegogmrealcred@gmail.com>
2025-02-18 03:28:06 +0000

    Primeira parte da construção do super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,68 +1,69 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste Diego
-
-
-
-
 int main(){
-    printf ("Cartas Super Trunfo\n");
-    printf ("Novo Commint\n");
 
+    printf("Cartas Super Trunfo\n");
+    
     int Carta = 1;
     char Estado = 'A';
-    float Codigo[12] = 'A01;
-    char nome [20] = "São Paulo";
-
+    char Codigo[4] = "A01";
+    char Nome[20] = "São Paulo";
 
     printf("Carta: %d\n", Carta);
     printf("Estado: %c\n", Estado);
-    printf("Codigo: %d\n", Codigo);
+    printf("Codigo: %s\n", Codigo);
     printf("Nome: %s\n", Nome);
 
- 
     /*
-   printf("%formato1 %formato2 %formato3 %formato4 %formato5 %formato6 %formato7 %formato8", variavel1 variavel2, variavel3, variavel4, variavel5, variavel6, variavel7, variavel8);
+printf("%formato1 %formato2 %formato3", variavel1 variavel2, variavel3, ...);
 
-    %d: Imprime um inteiro no formato decimal.
-    %i: Equivalente a %d.
-    %f: Imprime um número de ponto flutuante no formato parão.
-    %e: Imprime um número de ponto flutuante na notação científica.
-    %C: Imprime um único caractere.
-    %s: Imprime uma cadeia (string) de caracteres.
+%d: Imprime um inteiro no formato decimal.
+%i: Equivalente a %d.
+%f: Imprime um número de ponto flutuante no formato parão.
+%e: Imprime um número de ponto flutuante na notação científica.
+%C: Imprime um único caractere.
+%s: Imprime uma cadeia (string) de caracteres.
+*/
+
+return 0;
 
 
 
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    */
 
-    
 
-    return 0;
+
+
+
+
+
+
+
+
+
+
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
