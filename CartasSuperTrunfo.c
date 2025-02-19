3af0576273e7f57c3de9144594d7627c22576366
DiegoMarinho27 <diegogmrealcred@gmail.com>
2025-02-19 15:26:56 +0000

    construido a primeira parte CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,18 +4,37 @@ int main(){
 
     printf("Cartas Super Trunfo\n");
     
-    int Carta = 1;
-    char Estado = 'A';
-    char Codigo[4] = "A01";
-    char Nome[20] = "São Paulo";
+    int carta = 1;
+    int populacao = 11450000;
+    int numerodepontosturisticos = 36;
+    double area = 1.521;
+    float pibbilhoesdereais = 2.719751231; 
+    char estado = 'A';
+    char codigo[4] = "A01";
+    char nomedaCidade[20] = "São Paulo";
+     
+    
+   
 
-    printf("Carta: %d\n", Carta);
-    printf("Estado: %c\n", Estado);
-    printf("Codigo: %s\n", Codigo);
-    printf("Nome: %s\n", Nome);
+
+
+
+    printf ("Carta: %d\n", carta);
+    printf("Estado: %c\n", estado);
+    printf("Código: %s\n", codigo);
+    printf("Nome da Cidade: %s\n", nomedaCidade);
+    printf("População: %.2d\n", populacao);
+    printf("Área: %.3f km²\n", area);
+    printf("PIB: %.9f bilhões de reais\n", pibbilhoesdereais);
+    printf("Números de Pontos Turísticos: %d\n", numerodepontosturisticos); 
+    
+    
 
     /*
 printf("%formato1 %formato2 %formato3", variavel1 variavel2, variavel3, ...);
+
+// Multiplicando por 1000 para converter para km²
+    float areaEmKm2 = Areakm² * 1000;
 
 %d: Imprime um inteiro no formato decimal.
 %i: Equivalente a %d.
@@ -26,22 +45,6 @@ printf("%formato1 %formato2 %formato3", variavel1 variavel2, variavel3, ...);
 */
 
 return 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
 
