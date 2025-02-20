23136f7feaae2e1b56cffbfe8537df7078060f13
Jonathas Dias <83734773+wCosmic@users.noreply.github.com>
2025-02-20 22:48:56 +0000

    Criei a primeira cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,25 @@
 //Teste larissa
 
 int main() {
+
+    char estado[2] = "A";
+    char codigo[4] = "A01";
+    char nome[20] = "São Paulo";
+    int populacao = 10500035;
+    float area = 25000.00;
+    float pib = 310000000.00;
+    int pontos = 241;
+
+    printf("A carta está pronta!\n");
+    printf("Estado: %s\n", estado);
+    printf("Codigo: %s\n", codigo);
+    printf("Nome: %s\n", nome);
+    printf("Populacao: %d\n", populacao);
+    printf("Area: %.2f\n Km²", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Número de Pontos Turiscos: %d\n", pontos);
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
