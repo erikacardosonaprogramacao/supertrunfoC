7332e265c09eb61cfae1f2e153eb4be1f69cfb15
DiegoMarinho27 <diegogmrealcred@gmail.com>
2025-02-21 15:17:32 +0000

    Nivel intermediário Carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,14 +7,21 @@ int main(){
     
     
     
-    int idade = 1;                                      // Numero do estado representado em numeros de "1" a "8" 
-    int populacao = 11450000;                               // População da cidade
-    int quantidade = 36;                                        // Quantidade de idade
-    double area = 1.521;                                   // Área em km²
-    double pibbilhoesdereais = 2.719751231;                 // Produto interno bruto da cidade
-    char estado = 'A';                                     // Nome do estado representado por um letra de "A" a "H"
-    char codigo[4] = "A01";                                // Letra inicial do estado seguido de um numero ex: A01, A02, A03
-    char nomedacidade[12] = "São Paulo";                  // Nome da cidade
+    int idade = 1;// Numero do estado representado em numeros de "1" a "8"                                       
+    int populacao = 11450000;// Quantidade de idade                               
+    int quantidade = 36;// Quantidade de idade                                    
+    double area = 1.521;// Área em km²                                   
+    double pibbilhoesdereais = 2.719751231;// Produto interno bruto da cidade                 
+    char estado = 'A';// Nome do estado representado por um letra de "A" a "H"                                     
+    char codigo[4] = "A01";// Letra inicial do estado seguido de um numero ex: A01, A02, A03                                
+    char nomedacidade[12] = "São Paulo";// Nome da cidade                  
+
+    // Cálculo da densidade populacional
+    double densidadePopulacional = (double)populacao / area;
+
+    // Cálculo do PIB per capita
+    double pibPerCapita = (double)pibbilhoesdereais * 1e9 / populacao; // Convertendo PIB de bilhões para reais
+
      
     printf("Carta: %d\n", idade);
     printf("Estado: %c\n", estado);
@@ -24,9 +31,12 @@ int main(){
     printf("Área: %.3f km²\n", area);
     printf("PIB: %.9f bilhões de reais\n", pibbilhoesdereais);
     printf("Números de Pontos Turísticos: %d\n", quantidade);
+    printf("Densidade Populacional: %.2f habitantes/km²\n", densidadePopulacional);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita);
+    
 
    
-   
+   // Leitura de dados do usuário
    
     printf ("Carta: %d\n", idade);
     scanf("%d", &idade);
@@ -58,6 +68,8 @@ int main(){
 
     printf("Números de Pontos Turísticos: %d\n", quantidade);
     scanf("%d", &quantidade);
+
+
 
     
 
