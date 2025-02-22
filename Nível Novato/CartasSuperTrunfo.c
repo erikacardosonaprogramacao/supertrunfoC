645d0a47348bde88172f8c8da17159a186ca499b
Jooe Lisboa <jhow_vds@hotmail.com>
2025-02-22 14:03:45 +0000

    Mudanças Visual.

--- Nível Novato/CartasSuperTrunfo.c
+++ Nível Novato/CartasSuperTrunfo.c
@@ -18,8 +18,8 @@ int main(){
     // Cadastro da Carta 1 : 
     
                 //Estado
-    printf("Digite o nome do estado: ");
-    scanf("%sps", &estado1);               
+    printf("Digite a Sigla do estado: ");
+    scanf("%s", &estado1);               
                 //Código da carta
     printf("Digite o nome da carta: ");
     scanf("%s", &codigodacidade1);
@@ -39,19 +39,10 @@ int main(){
     printf("Digite a Quantidade de Pontos Turiscos: ");
     scanf("%d", &pontosturisticos1);
 
-    // Exibição dos Dados da Carta 1:
-
-                 //Vizualização da Carta Completa
-    printf("\n--- Dados da Carta %s ---\n", codigodacidade1);
-    printf("Estado: %s\nCódigo: %s\nNome da cidade: %s\n", estado1, codigodacidade1, nome1);
-    printf("População: %d\nÁrea: %.2fKM²\nPIB: %.2f\nNúmero de pontos turísticos: %d\n", populacao1, area1, pib1, pontosturisticos1);
-    printf("\n-------------------------\n");
-
-
      // Cadastro da Carta 2:
      
                  //Estado
-     printf("\nDigite o nome do estado: ");
+     printf("\nDigite a Sigla do estado: ");
      scanf("%s", &estado2);               
                  //Código da carta
      printf("Digite o nome da carta: ");
@@ -71,13 +62,23 @@ int main(){
                  //Numero de Pontos Turisticos
      printf("Digite a Quantidade de Pontos Turiscos: ");
      scanf("%d", &pontosturisticos2);
+
+
+      // Exibição dos Dados da Carta 1:
+
+                 //Vizualização da Carta Completa
+    printf("\n--- Dados da Carta %s ---\n", codigodacidade1);
+    printf("Estado: %s\nCódigo: %s\nNome da cidade: %s\n", estado1, codigodacidade1, nome1);
+    printf("População: %d\nÁrea: %.2fKM²\nPIB: %.2f Bilhões de Reais\nNúmero de pontos turísticos: %d\n", populacao1, area1, pib1, pontosturisticos1);
+    printf("\n-------------------------\n");
  
      // Exibição dos Dados da Carta 2:
+    
  
                   //Vizualização da Carta Completa
      printf("\n--- Dados da Carta %s ---\n", codigodacidade2);
      printf("Estado: %s\nCódigo: %s\nNome da cidade: %s\n", estado2, codigodacidade2, nome2);
-     printf("População: %d\nÁrea: %.2fKM²\nPIB: %.2f\nNúmero de pontos turísticos: %d\n", populacao2, area2, pib2, pontosturisticos2);
+     printf("População: %d\nÁrea: %.2fKM²\nPIB: %.2f Bilhões de Reais\nNúmero de pontos turísticos: %d\n", populacao2, area2, pib2, pontosturisticos2);
      printf("\n-------------------------\n");
      
  
