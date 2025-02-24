dc80306086a4a7cb7ef7e17f365307d404c1138d
Gabriel07 <gabrielfilipe15722@gmail.com>
2025-02-24 15:15:42 +0000

    Adicionado Código feito diretamente no vscode

---
+++ CartasSuperTrunfo1.c
@@ -0,0 +1,83 @@
+
+//Desafio SuperTrunfo Nivel inciante 
+
+#include <stdio.h>
+
+int main() {
+
+    //Declaração das variaveis da Carta 1
+
+    char Carta1 = 'A';
+    char Estado1 = 1;
+    int Codigo1 = 1;
+    char Cidade1[20] = "Aracaju";
+    int Populacao1 = 602.757;
+    float Area1 = 21.910;
+    float Pib1 = 51.086000;
+    int Numero_de_pontos_turisticos1 = 30;
+
+
+//Espaçamento
+
+printf("\n                                      \n");
+
+
+printf("\nCARTA1: \n");
+//Utilizando o Printf
+printf("\n                                      \n");
+printf("Carta: %c\n", Carta1);
+printf("Estado: %d\n", Estado1);
+printf("Codigo: %d\n", Codigo1);
+printf("Nome da cidade: %s\n", Cidade1);
+printf("População: %.d mil habitantes\n", Populacao1);
+printf("Area: %.3f km²\n", Area1);
+printf("PIB: %.6f Bilhões de reais\n", Pib1);
+printf("Número de Pontos turisticos: %d\n", Numero_de_pontos_turisticos1);
+
+
+
+
+
+
+
+//Declaração das variaveis da Carta 2
+
+char Carta2 = 'B';
+    char Estado2 = 2;
+    int Codigo2= 2;
+    char Cidade2[30] = "Maceió";
+    int Populacao2 = 957.916;
+    float Area2 = 509.320;
+    float Pib2 = 24.48401631;
+    int Numero_de_pontos_turisticos2 = 23;
+
+
+
+
+
+//Espaçamento    
+
+printf("\n                                      \n");
+
+
+printf("\nCARTA2: \n");
+
+//Utilizando o Printf
+printf("\n                                      \n");
+printf("Carta: %c\n", Carta2);
+printf("Estado: %d\n", Estado2);
+printf("Codigo: %d\n", Codigo2);
+printf("Nome da cidade: %s\n", Cidade2);
+printf("População: %.d mil habitantes\n", Populacao2);
+printf("Area: %.3f km²\n", Area2);
+printf("PIB: %.6f Bilhões de reais\n", Pib2);
+printf("Número de Pontos turisticos: %d\n", Numero_de_pontos_turisticos2);
+
+
+
+
+//Encerrando 
+
+return 0;
+
+}
